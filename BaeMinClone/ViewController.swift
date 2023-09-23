e69ff35c3dd3581b805b032612e5d125022c583c
dogyungkim <prunsoli12@gmail.com>
2023-09-23 15:33:17 +0900

    ING : Tabbar

---
+++ BaeMinClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaeMinClone
+//
+//  Created by 김도경 on 2023/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
